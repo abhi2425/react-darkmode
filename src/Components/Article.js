de411582d70ad4080efdf60e4e4cc995235d9978
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-05-25 18:59:00 +0530

    just adjusted few things

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import date from 'date-and-time'
-const Article = ({ topic, description, date: articleDate, readTime }) => {
-  return (
-    <article>
-      <h2>{topic}</h2>
-      <div>
-        <span
-          style={{
-            marginRight: '10px',
-            fontStyle: 'italic',
-            fontSize: '14px',
-            color: 'var(--color-font)',
-          }}
-        >
-          {date.format(articleDate, 'ddd, MMM DD YYYY')}
-        </span>
-        <span style={{ color: 'var(--color-font)' }}>{readTime}</span>
-      </div>
-      <p>{description}</p>
-    </article>
-  )
-}
+
+const Article = ({ topic, description, date: articleDate, readTime }) => (
+  <article>
+    <h2>{topic}</h2>
+    <div>
+      <span
+        style={{
+          marginRight: '10px',
+          fontStyle: 'italic',
+          fontSize: '14px',
+        }}
+      >
+        {date.format(articleDate, 'ddd, MMM DD YYYY')}
+      </span>
+      <span>{readTime}</span>
+    </div>
+    <p>{description}</p>
+  </article>
+)
 
 export default Article
